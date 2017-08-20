416fcd2a6dc04b3f8715eb42ad5265cf825978e2
Iskorka <camplesjeydi000@gmail.com>
2017-08-20 15:48:12 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,87 @@
 $(document).ready(function(){
+	
+	
 	$('#menu-icon').click(function(){
 		$('.mob-menu').toggleClass('hidden');
+	});
+	
+	
+	$('.bg-img2').hide();
+	$('.bg-img3').hide();
+	var n = 1;
+	
+	$('.forward').click(function(){
+		if (n === 1) {
+			$('.bg-img1').hide();
+			$('.bg-img2').show();
+			$('.banner li:first').removeClass('selected');
+			$('.banner li:nth-child(2)').addClass('selected');
+			n++;
+		}
+		else if (n === 2) {
+			$('.bg-img2').hide();
+			$('.bg-img3').show();
+			$('.banner li:nth-child(2)').removeClass('selected');
+			$('.banner li:last').addClass('selected');
+			n++;
+		}
+		else {
+			$('.bg-img3').hide();
+			$('.bg-img1').show();
+			$('.banner li:last').removeClass('selected');
+			$('.banner li:first').addClass('selected');
+			n = 1;
+		}
+	});
+	
+	$('.backward').click(function(){
+		if (n === 1) {
+			$('.bg-img1').hide();
+			$('.bg-img3').show();
+			$('.banner li:first').removeClass('selected');
+			$('.banner li:last').addClass('selected');
+			n = 3;
+		}
+		else if (n === 2) {
+			$('.bg-img2').hide();
+			$('.bg-img1').show();
+			$('.banner li:nth-child(2)').removeClass('selected');
+			$('.banner li:first').addClass('selected');
+			n--;
+		}
+		else {
+			$('.bg-img3').hide();
+			$('.bg-img2').show();
+			$('.banner li:last').removeClass('selected');
+			$('.banner li:nth-child(2)').addClass('selected');
+			n--;
+		}
+	});
+	
+	$('.banner li:first').click(function(){
+		$('.bg-img1').show();
+		$('.bg-img2').hide();
+		$('.bg-img3').hide();
+		$('.banner li:first').addClass('selected');
+		$('.banner li:nth-child(2)').removeClass('selected');
+		$('.banner li:last').removeClass('selected');
+	});
+	
+	$('.banner li:nth-child(2)').click(function(){
+		$('.bg-img1').hide();
+		$('.bg-img2').show();
+		$('.bg-img3').hide();
+		$('.banner li:first').removeClass('selected');
+		$('.banner li:nth-child(2)').addClass('selected');
+		$('.banner li:last').removeClass('selected');
+	});
+	
+	$('.banner li:last').click(function(){
+		$('.bg-img1').hide();
+		$('.bg-img2').hide();
+		$('.bg-img3').show();
+		$('.banner li:first').removeClass('selected');
+		$('.banner li:nth-child(2)').removeClass('selected');
+		$('.banner li:last').addClass('selected');
 	});
 });
